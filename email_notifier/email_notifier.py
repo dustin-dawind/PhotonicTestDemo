84fd25a1c0ba7cb91860b137ec2980c36e6a94b5
FREEDOM\mlarkins <mlarkins@freedomphotonics.com>
2024-09-25 14:19:15 -0700

    email_notifier.py initial commit

---
+++ email_notifier/email_notifier.py
@@ -0,0 +1,25 @@
+import smtplib
+from email.message import EmailMessage
+
+
+FROM = "testcompletionnotifier@gmail.com"
+PASSWORD = "qwnl vjwr inil ywbf"
+SUCCESS = "matthewdeanlarkins+test_complete@gmail.com"
+ERROR = "matthewdeanlarkins+test_error@gmail.com"
+
+
+def notify_success(test_name: str):
+    msg = EmailMessage()
+    msg["Subject"] = f"{test_name} Test Completed"
+    msg["From"] = FROM
+    msg["To"] = SUCCESS
+    msg.set_content("Test Successfully Completed")
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.ehlo()
+        smtp.login(FROM, PASSWORD)
+        smtp.send_message(msg)
+
+
+if __name__ == '__main__':
+    notify_success("Debugging")
