b0fc5b661d1f1dc69e70d26d31709daf84592a2b
FREEDOM\mlarkins <mlarkins@freedomphotonics.com>
2024-09-25 18:35:14 -0700

    latest update of liv_analysis.py

--- analysisfromtoml/analysis_scripts/__init__.py
+++ analysisfromtoml/analysis_scripts/__init__.py
@@ -1,1 +1,5 @@
 from analysisfromtoml.analysis_scripts.liv_analysis import liv_analysis
+
+__all__ = [
+    "liv_analysis",
+]
--- analysisfromtoml/analysis_scripts/liv_analysis.py
+++ analysisfromtoml/analysis_scripts/liv_analysis.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from pathlib import Path
+import numpy as np
+from scipy.interpolate import UnivariateSpline
 
 
 def liv_analysis(data_path: Path, analysis_flags: dict[str, bool], plot_livs: bool):
@@ -10,40 +12,69 @@ def plot_grouped_livs(df: pd.DataFrame,
                       analyze_by: list[str] | str,
                       unique_identifiers: list[str] | str,
                       ):
-    mean_df =df.groupby(by=analyze_by, as_index=False).mean()
+    grouped_df = df.groupby(by=analyze_by, as_index=False)
+    mean_df = grouped_df.mean()
+    std_df = grouped_df.std()
 
-    fig, axs = plt.subplots(1, 2)
-    for name, group in mean_df.groupby(by=unique_identifiers, as_index=False):
-        label = ''
-        if len(unique_identifiers) > 1:
-            for i, name_part in enumerate(name):
-                label += f"{unique_identifiers[i][0]}: {name_part} "
-        else:
-            label = f"{unique_identifiers[0][0]}: {name[0]}"
-        axs[0].plot(group["Measured Current (mA)"],
-                    group["Measured Power (mW)"],
-                    label=label
-                    )
-        axs[1].plot(group["Measured Current (mA)"],
-                    group["Efficiency"],
-                    )
+    for i, (name, group) in enumerate(mean_df.groupby(unique_identifiers, as_index=False)):
+        if i == 0:
+            y_spl = UnivariateSpline(group["Measured Current (mA)"],
+                                     group["Measured Power (mW)"],
+                                     s=0,
+                                     k=4
+                                     )
+            plt.plot(group["Measured Current (mA)"],
+                     group["Measured Power (mW)"],
+                     'ro'
+                     )
+            x_range = np.linspace(group["Measured Current (mA)"].iloc[0],
+                                  group["Measured Current (mA)"].iloc[-1],
+                                  1000
+                                  )
+            plt.plot(x_range,
+                     y_spl(x_range)
+                     )
+            plt.plot(x_range,
+                     y_spl.derivative(n=1)(x_range),
+                     'g'
+                     )
+            plt.plot(x_range,
+                     y_spl.derivative(n=2)(x_range),
+                     'k'
+                     )
+    plt.show()
 
-    fig.legend(loc="upper left")
-
-    return fig
+    # fig, axs = plt.subplots(1, 2)
+    # for name, group in mean_df.groupby(by=unique_identifiers, as_index=False):
+    #     label = ''
+    #     if len(unique_identifiers) > 1:
+    #         for i, name_part in enumerate(name):
+    #             label += f"{unique_identifiers[i][0]}: {name_part} "
+    #     else:
+    #         label = f"{unique_identifiers[0][0]}: {name[0]}"
+    #     axs[0].plot(group["Measured Current (mA)"],
+    #                 group["Measured Power (mW)"],
+    #                 label=label
+    #                 )
+    #     axs[1].plot(group["Measured Current (mA)"],
+    #                 group["Efficiency"],
+    #                 )
+    #
+    # fig.legend(loc="upper left")
+    #
+    # return fig
 
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
-    path = Path(r"C:\Users\Matt\PycharmProjects\Quintessent Presentation\resources\test_results\data.csv")
+    # path = Path(r"C:\Users\Matt\PycharmProjects\Quintessent Presentation\resources\test_results\data.csv")
+    path = Path(r"C:\Users\mlarkins\Local Data\Programming\Automation\resources\test_results\data.csv")
     test_df = pd.read_csv(path)
 
-    fig = plot_grouped_livs(test_df,
-                            analyze_by=["Wafer ID", "Field ID", "Set Current (mA)"],
-                            unique_identifiers=["Wafer ID", "Field ID"]
-                            )
-
-    plt.show()
+    plot_grouped_livs(test_df,
+                      analyze_by=["Wafer ID", "Field ID", "Set Current (mA)"],
+                      unique_identifiers=["Wafer ID", "Field ID"]
+                      )
 
     # # Groups all the data by wafer and field and then averages each set current for every device within each group
     # grouped_average = test_df.groupby(by=["Wafer ID", "Field ID", "Set Current (mA)"], as_index=False).mean()
